434253cd1eb43bef46ce519053e6c3070196740d
Luis Gabriel <luisgabrielcaetano@gmail.com>
2021-10-18 23:27:27 -0300

    changing some styles and refactoring the project links so that this can be deployed to netlify

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -2,8 +2,8 @@ import React from "react";
 import { Component } from "react";
 import calculadora from '../imgs/calculadora.jpg'
 import flappyBird from '../imgs/flappyBird.jpg'
-import projetoGaleria from '../imgs/projetoGaleria.jpg'
-import sudokuSolver from '../imgs/sudokuSolver.jpg'
+import galeria from '../imgs/galeria.jpg'
+import sudoku from '../imgs/sudoku.jpg'
 // // import { Router } from "react-router"
 // import {
 //     BrowserRouter as Router,
@@ -16,14 +16,18 @@ import sudokuSolver from '../imgs/sudokuSolver.jpg'
 
 export default class ProjectCard extends Component{
 
-    imagesObject = { calculadora, flappyBird, projetoGaleria, sudokuSolver }
+    imagesObject = { calculadora, flappyBird, galeria, sudoku, 
+        cadastro: "https://d33wubrfki0l68.cloudfront.net/616e1efaa6a3d37f46c8073e/screenshot_2021-10-19-01-29-19-0000.png",
+        countries: "https://d33wubrfki0l68.cloudfront.net/616e0cdfad504960c1379adc/screenshot_2021-10-19-00-11-19-0000.png"
+    }
 
     descriptionsObject = {
-        sudokuSolver: 'A web page wher you can play sudoku and solve any sudoku puzzle with the help of a backtracking algorithm',
+        sudoku: 'A web page wher you can play sudoku and solve any sudoku puzzle with the help of a backtracking algorithm',
         flappyBird: 'A clone of the original game for the browser written with pure JavaScript',
-        projetoGaleria: 'An online gallery project that was built as a part of a course I took on Udemy on jQuery and Bootstrap.',
-        calculadora: 'A browser based clone of the MacOS native calculator app.'
-
+        galeria: 'An online gallery project that was built as a part of a course I took on Udemy on jQuery and Bootstrap.',
+        calculadora: 'A browser based clone of the MacOS native calculator app.',
+        countries: 'A site that provides information about every country in the world and also provides a flag quiz so that you can test your geography knowledge!',
+        cadastro: 'A site that enables registration from users and is integrated with a Firebase server'
     }
 
 
@@ -36,7 +40,7 @@ export default class ProjectCard extends Component{
 
 
     render(){
-        return <a href={`/apps/${this.props.projectName}/index.html`}>
+        return <a href={`https://lgcaetano${this.props.projectName}.netlify.app`}>
             <div className="project-card">
                 <b>{this.props.projectName}</b>
                 <div className="image-container">
--- src/components/ProjectsPanel.jsx
+++ src/components/ProjectsPanel.jsx
@@ -18,12 +18,13 @@ export default class ProjectsPanel extends Component{
         return <div id="project-panel">
             <h1>My Projects</h1>
             <div id="project-card-grid">
-                <ProjectCard projectName="sudokuSolver" clickFunction={src => this.handleClick(src)}></ProjectCard>
+                <ProjectCard projectName="sudoku" clickFunction={src => this.handleClick(src)}></ProjectCard>
+                <ProjectCard projectName="countries" clickFunction={src => this.handleClick(src)}></ProjectCard>
+                <ProjectCard projectName="cadastro" clickFunction={src => this.handleClick(src)}></ProjectCard>
                 <ProjectCard projectName="flappyBird" clickFunction={src => this.handleClick(src)}></ProjectCard>
                 <ProjectCard projectName="calculadora" clickFunction={src => this.handleClick(src)}></ProjectCard>
-                <ProjectCard projectName="projetoGaleria" clickFunction={src => this.handleClick(src)}></ProjectCard>
+                <ProjectCard projectName="galeria" clickFunction={src => this.handleClick(src)}></ProjectCard>
             </div>
-            {/* <iframe title="shower" src={this.state.iFrameReference} style={{display: this.state.iFrameReference == '#' ? 'none' : 'initial'}}></iframe> */}
         </div>
     }
 }
