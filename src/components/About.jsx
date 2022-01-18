39e301f2139752f995978dda512467e1e446cd3d
Luis Gabriel <luisgabrielcaetano@gmail.com>
2022-01-18 20:11:41 -0300

    changed a few things on the about page, info was outdated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -10,8 +10,8 @@ export default function About(){
                 web developer. I absolutely love what I do and I am always looking for opportunities to get better at my craft. I love 
                 building personal projects, and I think many of them look pretty cool (check them out in my home page if you haven't already!).
                 <br />
-                The current technologies I've been working with the most are React (specially with the use of Next.js), Typescript, TailwindCSS
-                and Firebase.
+                The current technologies I've been working with the most are React (specially with the use of Next.js), TailwindCSS,
+                 MongoDB and Express. I've been doing some cool projects with Python too (check my <a href="https://github.com/lgcaetano">GitHub</a> if you wanna see any of those).
                 <br />
                 I also study Information Systems at UFMG in Belo Horizonte, Brazil. I'm currently on my 5th semester.  
             </p>
